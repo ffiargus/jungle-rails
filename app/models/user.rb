734b46266ecb42640776b902751a511575381503
ffiargus <rm.chow@outlook.com>
2017-06-12 20:08:58 +0000

    creates more tests for user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 
+  before_save :downcase_email
+
   has_secure_password
 
   validates :password, length: { minimum: 6 }
@@ -7,5 +9,20 @@ class User < ActiveRecord::Base
   validates :last_name, presence: true
   validates :email, presence: true,
                     uniqueness: { case_sensitive: false }
+
+  def self.authenticate_with_credentials(email, password)
+    @user = User.find_by_email(email.downcase.strip)
+    if @user.authenticate(password)
+      return @user
+    else
+      return nil
+    end
+  end
+
+  private
+
+  def downcase_email
+    self.email.downcase!
+  end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'User Validation' do
+  describe 'Validations' do
     it 'should not save user if pass and confirm do not match' do
       user = User.new email: 'l@l.com', first_name: 'Milind', last_name: 'Shah', password: '123456', password_confirmation: '234567'
       user.save
@@ -20,6 +20,28 @@ RSpec.describe User, type: :model do
       user = User.new email: 'l@l.com', first_name: 'Milind', last_name: 'Shah', password: '1', password_confirmation: '1'
       user.save
       expect(user.errors.full_messages).to eq(['Password is too short (minimum is 6 characters)'])
+    end
+
+  end
+
+  describe '.authenticate_with_credentials' do
+
+    it 'should log the user in with the correct password' do
+      user = User.new email: 'l@l.com', first_name: 'Milind', last_name: 'Shah', password: '123456', password_confirmation: '123456'
+      user.save!
+      expect(User.authenticate_with_credentials(user.email, user.password)).to eq(user)
+    end
+
+    it 'should authenticate with spaces added before or after email' do
+      user = User.new email: 'l@l.com', first_name: 'Milind', last_name: 'Shah', password: '123456', password_confirmation: '123456'
+      user.save!
+      expect(User.authenticate_with_credentials('l@l.com  ', user.password)).to eq(user)
+    end
+
+    it 'should authenticate with case insensitivity in email' do
+      user = User.new email: 'l@L.com', first_name: 'Milind', last_name: 'Shah', password: '123456', password_confirmation: '123456'
+      user.save!
+      expect(User.authenticate_with_credentials('l@l.COM', user.password)).to eq(user)
     end
 
   end
